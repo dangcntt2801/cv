7edda5e2cffd664c42aab8ba21eac6bbe0a2508a
Dang.HT <dang@marble-d.co.jp>
2022-02-27 22:46:45 +0700

    v1

--- src/main.js
+++ src/main.js
@@ -7,14 +7,13 @@ import http from './http/index';
 import auth from './plugins/auth.js';
 import VueGtag from "vue-gtag";
 
-createApp(App).use(VueGtag, {
-    config: { id: "G-EH8EXCV5VF" }
-}).mount("#app");
-
 createApp(App)
     .use(router)
     .use(i18n)
     .use(store)
     .use(http)
     .use(auth)
+    .use(VueGtag, {
+        config: { id: "G-EH8EXCV5VF" }
+    })
     .mount('#app')
